3faa1cadfb22fcc5565b41f4edbd2ed9bbd7859f
idfcX <sansan8970@gmail.com>
2025-08-23 03:53:20 +0200

    Update tusb_config.h

--- rp2350_uart_bridge/tusb_config.h
+++ rp2350_uart_bridge/tusb_config.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "tusb_option.h"
 
+#define CFG_TUSB_MCU OPT_MCU_RP2040   
+#define CFG_TUSB_OS  OPT_OS_PICO
+#define CFG_TUD_ENABLED   1
+#define CFG_TUD_CDC       1
+
+#define CFG_TUD_ENDPOINT0_SIZE 64
+#define CFG_TUD_CDC_RX_BUFSIZE 512
+#define CFG_TUD_CDC_TX_BUFSIZE 512
+#define CFG_TUD_CDC_EP_BUFSIZE 64
+
 // MCU/OS for Pico SDK (RP2350 uses the RP2040 TinyUSB port)
 #define CFG_TUSB_MCU OPT_MCU_RP2040
 #define CFG_TUSB_OS  OPT_OS_PICO
